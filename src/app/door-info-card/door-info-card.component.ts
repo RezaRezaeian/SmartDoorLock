ec12f290fe282ebf2c311d2b27b2f3ef4b153b63
HomePcReza <rreza.rezaeian@gmail.com>
2024-06-05 17:20:06 +0330

    add login-form-component

--- src/app/door-info-card/door-info-card.component.ts
+++ src/app/door-info-card/door-info-card.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-door-info-card',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './door-info-card.component.html',
-  styleUrl: './door-info-card.component.css'
+  styleUrl: './door-info-card.component.css',
 })
 export class DoorInfoCardComponent {
-
+  faCoffee = faCoffee;
 }
