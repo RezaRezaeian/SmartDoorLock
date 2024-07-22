6358608dac6e1d1de702b0ce4e772a8339d66dde
HomePcReza <rreza.rezaeian@gmail.com>
2024-07-23 02:41:30 +0330

    edit footer edit header and generate doors-list component

--- src/app/door-info-card/door-info-card.component.ts
+++ src/app/door-info-card/door-info-card.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-door-info-card',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, HeaderComponent, FooterComponent],
   templateUrl: './door-info-card.component.html',
   styleUrl: './door-info-card.component.css',
 })
 export class DoorInfoCardComponent {
   faCoffee = faCoffee;
+  doorState: boolean = false;
+  public openDoor() {
+    if (!this.doorState) {
+      this.doorState = true;
+    }
+  }
+  public closeDoor() {
+    if (this.doorState) {
+      this.doorState = false;
+    }
+  }
 }
